ec937cc07f62ff9a5b92fd9ef3600fef643e4150
Fabiogracia777 <>
2020-03-02 11:42:48 -0300

    atualização

--- src/cidadefantasy/Aeroporto.java
+++ src/cidadefantasy/Aeroporto.java
@@ -2,40 +2,71 @@ package cidadefantasy;
 
 import java.util.ArrayList;
 
-public class Aeroporto implements Observavel{
-    
+public class Aeroporto implements Observavel {
+
     ArrayList<Voo> listaVoo = new ArrayList<>();
     ArrayList<Observador> ListaDeEmbarque = new ArrayList<>();
     ArrayList<Observador> ListaDeDesembarque = new ArrayList<>();
 
     @Override
-    public void Notificar() {
-       
+    public void cadastrarTotemEmbarque(Observador ob) {
+        this.ListaDeEmbarque.add(ob);
+    }
+
+    @Override
+    public void removerTotemEmbarque(Observador ob) {
+        this.ListaDeEmbarque.remove(ob);
+    }
+
+    @Override
+    public void cadastrarTotemDesembarque(Observador ob) {
+        this.ListaDeDesembarque.add(ob);
+    }
+
+    @Override
+    public void removerTotemDesembarque(Observador ob) {
+        this.ListaDeDesembarque.remove(ob);
+    }
+
+    @Override
+    public void notificarTotem(Voo v) {
+        if (v.getTipo() == "Embarque") {
+            for (Observador emb : this.ListaDeEmbarque) {
+                emb.Atualizar(v);
+            }
+        } else {
+            if (v.getTipo() == "Desembarque") {
+                for (Observador desem : this.ListaDeDesembarque) {
+                    desem.Atualizar(v);
+                }
+            }
+        }
     }
     
-public void AdicionarVoo (Voo e){
-    this.listaVoo.add(e);
+    @Override
+    public void cadastrarVoo(Voo v){
+        this.listaVoo.add(v);
+        if (v.getTipo()=="Embarque"){
+            this.notificarTotem(v);
+        }else 
+            if (v.getTipo()=="Desembarque"){
+                this.notificarTotem(v);
+        }
+    }
+    
+    @Override
+    public void removerVoo(Voo v){
+        v.setAtivo(false);
+        if(v.getTipo()=="Embarque"){
+            this.ListaDeEmbarque.remove(v);
+        }else
+            if(v.getTipo()=="Desembarque") {
+                this.ListaDeDesembarque.remove(v);
+            }
+    }
+
+    @Override 
+    public void atualizarVoo(int numero){
+        
+    }
 }
-
-    @Override
-    public void AdicionarDesembarque() {
-        
-    }
-
-    @Override
-    public void RemoverEmbarque() {
-        
-    }
-
-    @Override
-    public void RemoverDesembarque() {
-        
-    }
-
-    @Override
-    public void AdicionarEmbarque(SalaDeEmbarque a) {
-        this.ListaDeEmbarque.add(a);
-    }
-    }
-
-
